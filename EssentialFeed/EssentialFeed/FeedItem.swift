3ef9cdee2db34464b566a8a21f48d900f172f7eb
Ashar Guglielmino <alex.gugz@gmail.com>
2023-12-21 13:37:10 +0800

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ashar Guglielmino on 21/12/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
